4d802b0aef746190ac39471ee9ba912abc5e8299
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-14 22:16:44 -0600

    Update

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
+
+console.log(titleCased());
